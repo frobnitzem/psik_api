5a1c48b881fbd1676d2faf6c6366a4b971474141
David M. Rogers <predictivestatmech@gmail.com>
2023-08-24 18:13:10 -0400

    Documented secure single-user run mode.

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
 poetry run uvicorn olcf_api.main:app --reload --log-level info
+
+# Secure options for single-user execution:
+#--uds $HOME/olcf_api.sock # process requests through a UNIX domain socket
+#--fd 0 # process requests through stdin/out
